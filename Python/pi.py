0585d2075ac4d4d42217dd37803cfee1bcfdd99c
hitesh-04 <75922163+hitesh-04@users.noreply.github.com>
2021-07-23 16:32:45 +0530

    Update pi.py

--- Python/pi.py
+++ Python/pi.py
@@ -8,23 +8,15 @@ num = int(input("Enter a number according to your computer status :"))
 a = 1
 
 d = 0
-x = num%2
-if(x==0):
-    x = num/50
-else:
-    x = (num+1)/50
 
-y = 1
-print("[", end="")
 for i in range(num):
     b = a+2
-    # print(f"a = {a}, b = {b}")
-    if ((x*y) == i):
-        print("#", end="")
-        y = y + 1
+    print(f"{a},{b},", end="")
+    
 
     c = 1/a - 1/b
     d = d + c
     a = a + 4
-print("]")
+
+print("")
 print("Value of pi is", 4*d)
